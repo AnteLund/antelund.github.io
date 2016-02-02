a526b8f935b6fc3c20c5247c0d2a03d14f34de5d
Lundsten <andreas.lundsten@capgemini.com>
2016-02-02 17:19:32 +0100

    cool

--- js/controllers.js
+++ js/controllers.js
@@ -1,33 +1,43 @@
-hsgApp.controller('StudentCreateController', function($scope,studentService) {
+hsgApp.controller('StudentCreateController', function($scope,$filter,studentService) {
 	$scope.yearOfGraduation = 'reset';
-		$scope.cityOfInterest ='reset';
+	$scope.cityOfInterest = 'reset';
 
 	var getCities = function(){
 		studentService.getCities().then(function(allCitites){
-			$scope.cities = allCitites;
-			console.log(allCitites);	
+      angular.forEach(allCitites, function(value,key){
+        value.selected = "false";
+      })
+      $scope.cities = allCitites;
+		});
+	};
+  getCities();
+
+	$scope.addStudent = function() {
+		studentService.createStudent(buildStudent()).then(function(response){
+			console.log(response);
+      resetForm();
 		});
 	};
 
-	getCities();
-	var emptyStudent = {firstName : '', lastName : '', email: ''}
-	$scope.addStudent = function() {
-		var student = {
-			firstName : $scope.firstName, 
-			lastName : $scope.lastName, 
-			email: $scope.email,
-			exam: $scope.exam,
-			yearOfGraduation: $scope.yearOfGraduation,
-			cityOfInterest: $scope.cityOfInterest};
-		studentService.createStudent(student).then(function(response){
-			alert("Student Created");
-		});
-		$scope.studentForm.$setPristine();
-		$scope.firstName = emptyStudent.firstName;
-		$scope.lastName = emptyStudent.lastName;
-		$scope.email = emptyStudent.email;
-		$scope.yearOfGraduation = 'reset';
-		$scope.cityOfInterest ='reset';
-	};
-  	
+  var buildStudent = function(){
+    var selectedCities = $filter('filter')($scope.cities, {selected:'true'});
+    var student = {
+      firstName : $scope.firstName,
+      lastName : $scope.lastName,
+      email: $scope.email,
+      exam: $scope.exam,
+      yearOfGraduation: $scope.yearOfGraduation,
+      cityOfInterest: selectedCities};
+    return student;
+  }
+
+  var resetForm = function(){
+    var emptyStudent = {firstName : '', lastName : '', email: '', yearOfGraduation: 'reset', cityOfInterest: 'reset'};
+    $scope.studentForm.$setPristine();
+    $scope.firstName = emptyStudent.firstName;
+    $scope.lastName = emptyStudent.lastName;
+    $scope.email = emptyStudent.email;
+    $scope.yearOfGraduation = emptyStudent.yearOfGraduation;
+    $scope.cityOfInterest = emptyStudent.cityOfInterest;
+  }
 });
