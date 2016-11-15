5b557fb3d7ff090c89be6456e6f844cc97a54039
Lundsten <andreas.lundsten@capgemini.com>
2016-11-15 10:25:51 +0100

    Changed to https server-request

--- js/services.js
+++ js/services.js
@@ -2,18 +2,18 @@ hsgApp.factory('studentService', function($http) {
   var studentService = {};
 
   studentService.getCities = function(callback) {
-    return $http.get('http://andreaslundsten.asuscomm.com:8080/api/cities').then(function(response) {
+    return $http.get('https://andreaslundsten.asuscomm.com:8080/api/cities').then(function(response) {
       return response.data;
     });
   };
 
   studentService.getRoles = function(callback) {
-    return $http.get('http://andreaslundsten.asuscomm.com:8080/api/roles').then(function(response) {
+    return $http.get('https://andreaslundsten.asuscomm.com:8080/api/roles').then(function(response) {
       return response.data;
     });
   };
   studentService.createStudent = function(student) {
-    return $http.post('http://andreaslundsten.asuscomm.com:8080/api/students',student).then(function(data,status,headers)
+    return $http.post('https://andreaslundsten.asuscomm.com:8080/api/students',student).then(function(data,status,headers)
     {
         console.log("Student Added");
         return data;
